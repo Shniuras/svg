0b2b5260c5e3b871df7fc8727098182877dc3de9
Shniuras <seimurinho@gmail.com>
2018-01-31 15:37:55 +0200

    Trapecija updated

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ class Text extends Tag{
 }
 
 class Rectangle extends Tag{
-    public function __construct($width, $height, $x, $y)
+    public function __construct($width, $height, $x, $y, $color)
     {
         $this->name = 'rect';
         $this->attributes = [
@@ -51,59 +51,87 @@ class Rectangle extends Tag{
             'y'         =>  $y,
             'width'     =>  $width,
             'height'    =>  $height,
-            'style'     => 'fill:red'
+            'style'     =>  "fill:" . $color
         ];
     }
 }
 
 class Circle extends Tag{
-    public function __construct($cx, $cy, $r)
+    public function __construct($cx, $cy, $r, $color, $stroke, $strokeWidth)
     {
         $this->name = 'circle';
         $this->attributes = [
             'cx' => $cx,
             'cy' => $cy,
             'r'  => $r,
-            'style'=> 'fill:green',
-            'stroke' => 'black',
-            'stroke-width' => 1
+            'style'=> "fill:" . $color,
+            'stroke' => $stroke,
+            'stroke-width' => $strokeWidth
         ];
     }
 }
 
 class Triangle extends Tag {
-    public function __construct($a, $b, $c)
+    public function __construct($a, $b, $c, $color)
     {
         $this->name = 'polygon';
         $this->attributes = [
             'points' => $a->x . "," . $a->y . " " . $b->x . "," . $b->y . " " . $c->x . "," . $c->y,
-            'style' => 'fill:lime'
+            'style' => "fill:" . $color
         ];
     }
 }
 
 class Trapecija extends Tag {
-    public function __construct($a, $b, $c, $d)
+    public function __construct($a, $b, $c, $d, $color)
     {
         $this->name = 'polygon';
         $this->attributes = [
             'points' => $a->x . "," . $a->y . " " . $b->x . "," . $b->y . " " . $c->x . "," . $c->y . " " . $d->x . "," . $d->y,
-            'style' => "fill:orange"
+            'style' => "fill:" . $color
         ];
     }
 }
 
-class Trapeze extends Tag{
-    public function __construct()
+class Trapecija2 extends Tag{
+
+    public $rectangular;
+    public $triangleR;
+    public $triangleL;
+
+    public function __construct($x, $y, $height, $widthTri, $widthRec, $color)
     {
+        $startPointX = $x+($widthTri - $widthRec)/2;
+        $startPointY = $y-$height;
+        $endPointX = $x+$widthTri-($widthTri - $widthRec)/2;
+        $endPointX2 = $x + $widthTri;
+
+        // Keturkampis
+        $rect = new Rectangle($widthRec, $height, $startPointX, $startPointY, $color);
+        $this->rectangular = $rect;
+
+        // Trikampis is kaires
+        $trianL = new Triangle(new Points($x, $y), new Points($startPointX, $y), new Points($startPointX,$startPointY), $color);
+        $this->triangleL = $trianL;
+
+        // Trikampis is desines
+        $trianR = new Triangle(new Points($endPointX, $y), new Points($endPointX, $startPointY), new Points($endPointX2, $y), $color);
+        $this->triangleR = $trianR;
 
     }
+
+    public function draw(){
+        $this->rectangular->draw();
+        $this->triangleL->draw();
+        $this->triangleR->draw();
+    }
 }
+
 // 1.
-$rect = new Rectangle(250,150,400,250);
-$circle = new Circle (525, 325, 60);
-$triangle = new Triangle(new Points(200,400),new Points(400,250), new Points(400,400));
-$triangle2 = new Triangle(new Points(650,250),new Points(650,400), new Points(850,400));
+$rect = new Rectangle(250,150,400,250, "red");
+$circle = new Circle (525, 325, 60, "green", "black", 1);
+$triangle = new Triangle(new Points(200,400),new Points(400,250), new Points(400,400), "lime");
+$triangle2 = new Triangle(new Points(650,250),new Points(650,400), new Points(850,400), "lime");
 $firstText = new Text(478,328, "Labas Vakaras");
 
 echo '<svg width="1000" height="500">';
@@ -116,11 +144,19 @@ echo '</svg>';
 echo "<hr>";
 
 // 2.
-$trapecija = new Trapecija(new Points(450,0),new Points(650,0), new Points(800,400), new Points(300,400));
+$trapecija = new Trapecija(new Points(450,0),new Points(650,0), new Points(800,400), new Points(300,400), "orange");
 $secondText = new Text (490,100,"Piesiame trapecija");
 echo '<svg width="1000" height="400">';
 $trapecija->draw();
 $secondText->draw();
 echo '</svg>';
 echo "<hr>";
+
+// 2.1
+$newTrapecija = new Trapecija2(50,150,500,600,300, "orange");
+echo '<svg width="1000" height="400">';
+echo $newTrapecija->draw();
+echo '</svg>';
+
+// 3.
 
